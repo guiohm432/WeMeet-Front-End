72239268c2030c1dcacd618880dd67b51970278e
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-10-20 14:10:17 +0200

    FRONT SET UP MODIFICATIONS
    Add react-router-dom
    Move from js to jsx for App and index files
    Create folders structure
    Create a simple home page to test renderinig

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+// import UserProvider from "./components/Auth/UserProvider";
+import App from "./App";
+
+import "./App.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    {/* <UserProvider> */}
+      <App />
+    {/* </UserProvider> */}
+  </BrowserRouter>,
+  document.getElementById("root")
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () => {
+  
+
+  return (
+    <React.Fragment>
+      <h1>HELLO WEMEET</h1>
+    </React.Fragment>
+  );
+};
+
+export default Home;
